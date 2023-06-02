2f44284c0078d303a65ba7eed15be050c8b79ac4
Chikavi1 <chikavi@hotmail.com>
2023-06-01 22:13:58 -0500

    faq

--- resources/views/home/faq.blade.php
+++ resources/views/home/faq.blade.php
@@ -9,9 +9,9 @@ rel="stylesheet"
         <div class="pt-16 mb-16 lg:mb-0 lg:pt-32 lg:max-w-lg lg:pr-5">
           <div class="max-w-xl mb-6">
             <div>
-              <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+              <a href="/" class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
                 Radi Pets
-              </p>
+              </a>
             </div>
             <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
                 Preguntas Frecuentes
@@ -38,6 +38,15 @@ rel="stylesheet"
     <section class="px-4 py-24 mx-auto max-w-7xl">
         <div class="grid grid-cols-1 gap-0 text-gray-600 md:grid-cols-2 md:gap-16">
           <div>
+            <h5 class="mt-10 mb-3 font-semibold text-gray-900">¿Que hacer en caso de un error en la plataforma?, o alguna información incorrecta?</h5>
+            <p>
+                Si encuentras algún error en la plataforma de Radi Pets o identificas información incorrecta, es recomendable comunicarte con el equipo de Radi Pets para informarles sobre el problema. <br><br>
+                Al comunicar el error o la información incorrecta, proporciona detalles específicos sobre el problema que has encontrado y, si es posible, incluye capturas de pantalla o ejemplos que ayuden a ilustrar la situación. Esto permitirá al equipo de Radi Pets investigar y resolver el problema de manera más eficiente.
+                <br><br>
+
+                Recuerda que tu contribución al informar sobre errores o posibles mejoras es importante para garantizar que Radi Pets ofrezca la mejor experiencia posible tanto para los albergues como para los posibles adoptantes. <br><br>
+                Radi Pets valora el feedback y la participación de los usuarios para mejorar continuamente la plataforma.
+            </p>
             <h5 class="mt-10 mb-3 font-semibold text-gray-900">¿Cómo puedo crear un perfil para mi mascota en Radi Pets?</h5>
             <p>
               Para crear el perfil de tu mascota dirigite al apartado de "mis mascotas", selecciona la opcion de crear e ingresa la información solicitada, para más detalle sigue el siguiente <a class="font-bold text-purple-900" href="/tutorials/1">tutorial</a>.
@@ -56,18 +65,14 @@ rel="stylesheet"
                 Historia médica: Si tu mascota tiene alguna condición médica relevante,enfermedades cronicas, alergias o requerimientos especiales de cuidado, es importante mencionarlos.
                 <br><br>
                 Esterilización/Castración: Indica si tu mascota ha sido esterilizada o castrada.</p>
-            <h5 class="mt-10 mb-3 font-semibold text-gray-900">¿Cómo puedo cerrar el proceso de adopción una vez que encuentre un hogar adecuado para mi mascota?</h5>
-            <p>
-              Yes. We are a startup too, and we would love to be with you on this journey. There is a $10/month plan for startups and educational institutions. Check your eligibility
-              <a href="https://help.hellonext.co/faq/startup-eligibility/" target="_blank">here</a>.
-            </p>
+
             <h5 class="mt-10 mb-3 font-semibold text-gray-900">¿Puedo subir varias fotos de mi mascota en su perfil?
             </h5>
             <p>
                 Actualmente, en Radi Pets, solo se permite subir una foto por perfil de mascota. Sin embargo, estamos constantemente trabajando para mejorar y agregar nuevas funcionalidades a la plataforma. En un futuro, se está considerando la posibilidad de aumentar el número de fotos que se pueden subir al perfil de cada mascota. Asegúrate de estar atento a las actualizaciones y mejoras de Radi Pets para aprovechar al máximo las opciones disponibles.
             </p>
             <h5 class="mt-10 mb-3 font-semibold text-gray-900">¿Cómo puedo actualizar la información en el perfil de mi mascota?</h5>
-            <p>Yes. No strings attached. Free forever.</p>
+            <p>Dirígete al perfil de la mascota que deseas editar, y selecciona la opción de editar, ahí encontraras un formulario que te permitirá modificar los datos de la mascota.</p>
           </div>
           <div>
             <h5 class="mt-10 mb-3 font-semibold text-gray-900">¿Puedo crear perfiles para diferentes tipos de mascotas, como perros, gatos, aves, etc.?</h5>
@@ -76,12 +81,17 @@ rel="stylesheet"
             </p>
             <h5 class="mt-10 mb-3 font-semibold text-gray-900">¿Cómo puedo comunicarme con las personas interesadas en adoptar a mi mascota?
             </h5>
-            <p>Yes. Only for enterprise customers.</p>
+            <p>Cuando una persona está interesada en adoptar a tu mascota y al registrarse a través de Radi Pets, la plataforma facilita la comunicación al proporcionar la información de contacto del usuario interesado. Durante el proceso de registro, los posibles adoptantes ingresan su información de contacto, que puede incluir su nombre, número de teléfono y dirección de correo electrónico.</p>
             <h5 class="mt-10 mb-3 font-semibold text-gray-900">¿Cuánto tiempo tengo para decidir si acepto una solicitud de adopción o no?</h5>
-            <p>It will be a sad day for our team, and we would do whatever it takes to make you our happiest customer. But if you still insist, you can.</p>
+            <p>
+                En Radi Pets, el tiempo para decidir si aceptas una solicitud de adopción o no puede variar dependiendo de la dinámica y las políticas de cada albergue. Sin embargo, como recomendación general, se sugiere que se tome una decisión dentro de un plazo de menos de 2 semanas. <br><br> Este tiempo permite realizar una evaluación adecuada de la solicitud y brindar una respuesta oportuna a los posibles adoptantes.
+                <br><br>
+                Es importante tener en cuenta que, actualmente, las solicitudes de adopción son manejadas directamente por el albergue. Sin embargo, en un futuro, se está considerando la opción de permitir que las personas interesadas envíen solicitudes de adopción a través de la plataforma. Esto puede agilizar el proceso y permitir una mayor participación de los posibles adoptantes.
+<br><br>
+                Recuerda que, independientemente del tiempo establecido, es esencial tomar una decisión basada en el bienestar de la mascota y en la idoneidad del posible hogar adoptivo. Tomarse el tiempo necesario para evaluar las solicitudes y asegurarse de que la mascota se entregue a un ambiente adecuado es fundamental para garantizar su bienestar a largo plazo.</p>
             <h5 class="mt-10 mb-3 font-semibold text-gray-900">¿Hay alguna tarifa o costo asociado con el uso de Radi Pets como albergue?</h5>
             <p>
-              There are multiple support channels for us. If you prefer email, please write to support@hellonext.co. If you prefer chat support, please
+                ¡Así es! El uso de Radi Pets como albergue es completamente gratuito. No hay tarifas ni costos asociados para crear perfiles de mascotas, recibir solicitudes de adopción o utilizar las funciones disponibles en la plataforma. Radi Pets tiene como objetivo apoyar la causa de la adopción de mascotas y facilitar la conexión entre albergues y posibles adoptantes de forma accesible y sin costo alguno.
               <a href="#" class="underline">click here</a>.
             </p>
           </div>
@@ -90,3 +100,5 @@ rel="stylesheet"
 
 
 </body>
+
+@include('layouts.footer')
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -10,6 +10,9 @@
             <a href="https://www.radi.pet/privacy" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
                 Politicas de privacidad
             </a>
+            <a href="/faq" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                Preguntas frecuentes
+            </a>
 
              <!-- <a href="/adoptar" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
                 Adoptar
